ab78663f3454d58f7291846b97bd48c8bb821368
Vanshika Alang <vanshikaalang@Vanshikas-MacBook-Air.local>
2022-05-19 23:21:09 +0530

    First Commit

---
+++ 1.HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+// This is my first C++ programme.
+//  multi line comment = /* ...*/
+// line // to all line  =select and cmmd +/
+// comment
+int main(){
+    cout<<"Hello World";
+    // << is called insertion operator
+    // >> is called extraction operator
+    // cout<<endl   is same as \n
+    return 0;
+}
